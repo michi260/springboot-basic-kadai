24f43220e6e98170a834d388027e2493fa447dc2
michi260 <michiru260@docomo.ne.jp>
2025-03-19 20:31:52 +0900

    Delete springboot/spring_kadai_todo directory

--- springboot/spring_kadai_todo/src/main/java/com/example/springtutorial/repository/ToDoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springtutorial.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springtutorial.entity.ToDo;
-
-public interface ToDoRepository extends JpaRepository<ToDo,Integer>{
-	
-}
